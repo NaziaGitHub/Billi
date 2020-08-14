d09a16e30994b022380992b2671d29310b6d73ec
fahad <myjira17@gmail.com>
2020-08-14 03:09:06 -0700

    updated by fahad

---
+++ crm/src/test/java/orange/crm/NewTest.java
@@ -0,0 +1,27 @@
+package orange.crm;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class NewTest 
+{
+  @Test
+  public void f() throws Exception 
+  {
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("deviceName", "R9ZN60WKQ6R");
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("platformVersion", "10");
+		caps.setCapability("browserName", "Chrome");
+		caps.setCapability("chromedriverExecutable","D:\\Desktop and Download Data\\Backup Data\\Chrome Driver New\\ChromeDriver83\\chromedriver_win32\\chromedriver.exe");
+		AndroidDriver<AndroidElement> driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+		driver.get("https://www.rediff.com");
+  }
+}
